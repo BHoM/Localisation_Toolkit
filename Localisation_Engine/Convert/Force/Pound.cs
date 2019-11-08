fea226af61cbbd2ae2090c926af87e9c7ea888c2
Josef Taylor <josef.taylor@burohappold.com>
2019-11-08 14:21:23 -0500

    Added US Mass and Force units

---
+++ Localisation_Engine/Convert/Force/Pound.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2019, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+using BH.oM.Reflection.Attributes;
+
+namespace BH.Engine.Localisation.Length
+{
+    public static partial class Convert
+    {
+        [Description("Convert pound force into SI units (Newton)")]
+        [Input("pound", "Pound force measurement")]
+        [Output("si", "Pound force represented as SI units (Newton)")]
+        public static double FromPoundF(this double pound)
+        {
+            return pound * 4.4482216152605;
+        }
+
+        [Description("Convert SI units (Newton) into pound force")]
+        [Input("si", "SI measurement (Newton)")]
+        [Output("pound", "SI units (Newton) represented as pound force")]
+        public static double ToPoundF(this double si)
+        {
+            return si / 4.4482216152605;
+        }
+    }
+}
---
+++ Localisation_Engine/Convert/Mass/Pound.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2019, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+using BH.oM.Reflection.Attributes;
+
+namespace BH.Engine.Localisation.Length
+{
+    public static partial class Convert
+    {
+        [Description("Convert pound mass into SI units (kg)")]
+        [Input("pound", "Pound mass measurement")]
+        [Output("si", "Pound mass represented as SI units (kg)")]
+        public static double FromPoundM(this double pound)
+        {
+            return pound * 0.45359237;
+        }
+
+        [Description("Convert SI units (kg) into pound")]
+        [Input("si", "SI measurement (kg)")]
+        [Output("pound", "SI units (kg) represented as pound")]
+        public static double ToPoundM(this double si)
+        {
+            return si / 0.45359237;
+        }
+    }
+}
