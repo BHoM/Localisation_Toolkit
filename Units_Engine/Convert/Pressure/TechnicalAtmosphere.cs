4d22beec56ddc7e4e50dcf098da4cdfdcb3db79d
Peter Nugent <Peter.Nugent@burohappold.com>
2020-04-24 22:55:21 +0100

    Added To/From convert methods atmosphere, bar, kilogramforcepersquaremetre, kilogramforcepersqaurecentimeter, kilogramfrocepersquaremillimetre, newtonpersquarecentimetre, newtonpersquaremillimetre, technicalatmosphere, poundforcepersquareinch, poundforcepersquarefoot, tonneforcepersquaremillimetre, tonneforcepersquaremetre, tonneforcepersqaurecentimetre

---
+++ Units_Engine/Convert/Pressure/TechnicalAtmosphere.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UN = UnitsNet; //This is to avoid clashes between UnitsNet quantity attributes and BHoM quantity attributes
+using UnitsNet.Units;
+
+using System.ComponentModel;
+using BH.oM.Reflection.Attributes;
+using BH.oM.Quantities.Attributes;
+
+namespace BH.Engine.Units
+{
+    public static partial class Convert
+    {
+        [Description("Convert SI units (Pascals) into technical atmospheres")]
+        [Input("pascals", "The number of Pascals to convert", typeof(Pressure))]
+        [Output("technicalAtmospheres", "The number of technical atmospheres")]
+        public static double ToTechnicalAtmosphere(double pascals)
+        {
+            UN.QuantityValue qv = pascals;
+            return UN.UnitConverter.Convert(qv, PressureUnit.Pascal, PressureUnit.TechnicalAtmosphere);
+        }
+
+        [Description("Convert technical atmospheres into SI units (Pascals)")]
+        [Input("technicalAtmospheres", "The number of technical atmospheres to convert")]
+        [Output("pascals", "The number of Pascals", typeof(Pressure))]
+        public static double FromTechnicalAtmosphere(double technicalAtmospheres)
+        {
+            UN.QuantityValue qv = technicalAtmospheres;
+            return UN.UnitConverter.Convert(qv, PressureUnit.TechnicalAtmosphere, PressureUnit.Pascal);
+        }
+    }
+}
