2bb34f6193b16341c5f8540025642e093da2a667
Peter Nugent <Peter.Nugent@burohappold.com>
2020-05-20 12:11:53 +0100

    Added to/from methods kilogram-force centiemtres, kilogram-force metre, kilogram-force centimetre, Newton-centimetre, pound-force foot, pound-force inch, tonne-force centimetre, tonne-force metre, tonne-force millimetre

---
+++ Units_Engine/Convert/Torque/KilogramForceMetre.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UN = UnitsNet; //This is to avoid clashes between UnitsNet quantity attributes and BHoM quantity attributes
+using UnitsNet.Units;
+
+using System.ComponentModel;
+using BH.oM.Reflection.Attributes;
+using BH.oM.Quantities.Attributes;
+using UnitsNet;
+
+namespace BH.Engine.Units
+{
+    public static partial class Convert
+    {
+        [Description("Convert SI units (Newton-metres) into kilogram-force metres")]
+        [Input("newtonMetres", "The number of Newton-metres to convert", typeof(Moment))]
+        [Output("kilogramForceMetres", "The number of kilogram-force metres")]
+        public static double ToKilogramForceMetre(this double newtonMetres)
+        {
+            UN.QuantityValue qv = newtonMetres;
+            return UN.UnitConverter.Convert(qv, TorqueUnit.NewtonMeter, TorqueUnit.KilogramForceMeter);
+        }
+
+        [Description("Convert kilogram-force metre into SI units (Newton-metres)")]
+        [Input("kilogramForceMetres", "The number of kilogram-force metres to convert")]
+        [Output("newtonMetres", "The number of Newton-metres", typeof(Moment))]
+        public static double FromKilogramForceMetre(this double kilogramForceMetres)
+        {
+            UN.QuantityValue qv = kilogramForceMetres;
+            return UN.UnitConverter.Convert(qv, TorqueUnit.KilogramForceMeter, TorqueUnit.NewtonMeter);
+        }
+    }
+}
