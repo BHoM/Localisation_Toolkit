b992e4e8f2075430f94e628926b181c816fbcc58
Peter Nugent <Peter.Nugent@burohappold.com>
2020-05-20 11:50:04 +0100

    Conver to/from NewtonMillimetres

---
+++ Units_Engine/Convert/Torque/NewtonMillimetre.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UN = UnitsNet; //This is to avoid clashes between UnitsNet quantity attributes and BHoM quantity attributes
+using UnitsNet.Units;
+
+using System.ComponentModel;
+using BH.oM.Reflection.Attributes;
+using BH.oM.Quantities.Attributes;
+using UnitsNet;
+
+namespace BH.Engine.Units
+{
+    public static partial class Convert
+    {
+        [Description("Convert SI units (Newton-metres) into Newton-millimetres")]
+        [Input("newtonMetres", "The number of Newton-metres to convert", typeof(Moment))]
+        [Output("newtonMillimetre", "The number of Newton-millimetres")]
+        public static double ToNewtonMillimetre(this double newtonMetres)
+        {
+            UN.QuantityValue qv = newtonMetres;
+            return UN.UnitConverter.Convert(qv, TorqueUnit.NewtonMeter, TorqueUnit.NewtonMillimeter);
+        }
+
+        [Description("Convert Newton-millimetres into SI units (Newton-metres)")]
+        [Input("newtonMillimetres", "The number of Newton-millimetres to convert")]
+        [Output("newtonMetres", "The number of Newton-metres", typeof(Moment))]
+        public static double FromNewtonMillimetre(this double newtonMillimetres)
+        {
+            UN.QuantityValue qv = newtonMillimetres;
+            return UN.UnitConverter.Convert(qv, TorqueUnit.NewtonMillimeter, TorqueUnit.NewtonMeter);
+        }
+    }
+}
