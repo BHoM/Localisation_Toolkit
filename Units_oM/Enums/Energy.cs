c7c811eb1ef18d7a5821b0688ce8b06d2e1bdd63
Thomas Edward Kingstone <Tom.Kingstone@burohappold.com>
2024-02-20 14:38:29 +0000

    added units enum for enerfy and generic convert method

---
+++ Units_oM/Enums/Energy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BH.oM.Units
+{
+    public enum EnergyUnit
+    {
+        Undefined,
+        BritishThermalUnit,
+        Joule,
+        KilobritishThermalUnit,
+        Kilojoule,
+        KilowattHour,
+        MegabritishThermalUnit,
+        Megajoule,
+        MegawattHour,
+        WattHour,
+    }
+}
